5cef2cdb0c888b96e331e98d7c448c0939975b6e
TheMilkman3 <keithatarrant@gmail.com>
2015-01-24 00:09:19 -0700

    00:08 1/24/2015
    Entity class skeleton.

---
+++ GameJam2015/Entity.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GameJam2015
+{
+    class Entity
+    {
+        public Texture2D Sprite;
+        public Vector2 Position;
+        public Vector2 Size
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Performs initialization on entity.
+        /// </summary>
+        public void Initialize()
+        {
+        }
+
+        /// <summary>
+        /// Per frame update of entity.
+        /// </summary>
+        public void Update()
+        {
+        }
+
+        /// <summary>
+        /// Draws entity's sprite.
+        /// </summary>
+        public void Draw()
+        {
+        }
+    }
+}
