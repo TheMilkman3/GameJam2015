f234a999acbdb64760d082b999df298736775b82
virusgauge <mattj.pirtle@gmail.com>
2015-01-24 00:11:39 -0700

    Start of controller input

---
+++ GameJam2015/ControllerListener.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.GamerServices;
+
+namespace GameJam2015
+{
+    class ControllerListener
+    {
+        public ControllerListener()
+        {
+        }
+
+        
+    }
+}
