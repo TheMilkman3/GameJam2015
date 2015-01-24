8dabc5c01816192b6247c85f1358e2e82a7a3f7a
virusgauge <mattj.pirtle@gmail.com>
2015-01-24 14:07:06 -0700

    Audio Manager and test files

---
+++ GameJam2015/AudioManager.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Media;
+using System.IO;
+using System.Resources;
+
+namespace GameJam2015
+{
+    public class AudioManager
+    {
+        public SoundPlayer player;
+        public string stream, directory;
+        public Stream s;
+
+        public AudioManager(string path)
+        {
+            stream = path + "\\Audio";
+            directory = Path.GetFullPath(stream);
+
+            if (!Directory.Exists(directory))
+            {
+                Console.WriteLine("does not exist");
+            }
+            else
+            {
+                Console.WriteLine("Audio directory: {0}", directory);
+            
+            }
+            player = new SoundPlayer();
+            LoadAudio();
+        }
+
+        private void LoadAudioComplete()
+        {
+        }
+
+        private void PlayAudioFromResource(Object sender, EventArgs e)
+        {
+        }
+
+        private void LoadAudio()
+        {
+            foreach (string f in Directory.EnumerateFiles(directory))
+            {
+                try
+                {
+
+                    player.LoadAsync();
+                    Console.WriteLine("Audio load for {0} was successful.", f);
+                }
+                catch
+                {
+                    Console.WriteLine("Error loading");
+                }
+            }
+        }
+    }
+}
