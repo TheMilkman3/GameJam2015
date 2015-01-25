8c2c0a2424a94033af047b5dd187438bdb579744
TheMilkman3 <keithatarrant@gmail.com>
2015-01-25 11:23:09 -0700

    11:22 1/25/2015

--- GameJam2015/DeadlyDoodad.cs
+++ GameJam2015/DeadlyDoodad.cs
@@ -16,12 +16,13 @@ namespace GameJam2015
         static readonly int CHANGE_TIME = 1000;
         static readonly int DOODAD_SPEED = 2;
         int timeUntilChange = CHANGE_TIME;
-        int AIState = 0;
+        Random rand;
 
         public override void Initialize(Texture2D texture, float scale, Vector2 position)
         {
             base.Initialize(texture, scale, position);
             Velocity.X = DOODAD_SPEED;
+            rand = new Random();
         }
 
         public override List<Entity> Update(List<Entity> entities, GameTime gameTime)
@@ -29,27 +30,16 @@ namespace GameJam2015
             timeUntilChange -= gameTime.ElapsedGameTime.Milliseconds;
             if (timeUntilChange <= 0)
             {
-                switch (AIState)
+                int n = rand.Next(2);
+                if (n == 0)
                 {
-                    case 0:
-                        RotateCW();
-                        RotateCW();
-                        Console.WriteLine("X: " + Velocity.X + ", Y: " + Velocity.Y);
-                        break;
-                    case 1:
-                        RotateCW();
-                        Console.WriteLine("X: " + Velocity.X + ", Y: " + Velocity.Y);
-                        break;
+                    RotateCW();
+                }
+                else if (n == 1)
+                {
+                    RotateCCW();
                 }
                 timeUntilChange = CHANGE_TIME;
-                if (AIState == 1)
-                {
-                    AIState = 0;
-                }
-                else
-                {
-                    AIState++;
-                }
             }
             return base.Update(entities, gameTime);
 
